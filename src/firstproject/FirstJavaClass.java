a87c24ac1b30cbeb0614cf6ea7d7203fccd908ac
mfaisal <mfaisalpk@outlook.com>
2018-02-25 16:34:32 +0500

    Datatypes Done

--- src/firstproject/FirstJavaClass.java
+++ src/firstproject/FirstJavaClass.java
@@ -1,22 +1,40 @@
 package firstproject;
 import java.io.*;
+//using namespace System.out;
 
 public class FirstJavaClass {
 
 	public static void main(String[] args) {
 		
+//		DATA TYPES
+		byte temp = 20;
+		short temp1 = 20000;
+		int decimal = 2000000;
+		int octa = 0123;
+		int hexdecimal = 0xFF;
+		long temp3 = 2000000l;
+		float temp4 = 10.0f;
+		double temp5 = 10.0000;
+		boolean temp6 = true;
+		char temp7 = 'f';
+		String str = "mfaisal";
+		
+		System.out.println(str);
+		
+		
+		
 		/* Create two objects using constructor */
-		Employee empOne = new Employee("James Smith");
-		Employee empTwo = new Employee("Mary Anne");
-		// Invoking methods for each object created
-		empOne.empAge(26);
-		empOne.empDesignation("Senior Software Engineer");
-		empOne.empSalary(1000);
-		empOne.printEmployee();
-		empTwo.empAge(21);
-		empTwo.empDesignation("Software Engineer");
-		empTwo.empSalary(500);
-		empTwo.printEmployee();
+//		Employee empOne = new Employee("James Smith");
+//		Employee empTwo = new Employee("Mary Anne");
+//		// Invoking methods for each object created
+//		empOne.empAge(26);
+//		empOne.empDesignation("Senior Software Engineer");
+//		empOne.empSalary(1000);
+//		empOne.printEmployee();
+//		empTwo.empAge(21);
+//		empTwo.empDesignation("Software Engineer");
+//		empTwo.empSalary(500);
+//		empTwo.printEmployee();
 
 //		Singleton s= Singleton.getInstance();
 //		s.demo();
